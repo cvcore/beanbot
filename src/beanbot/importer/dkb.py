74f9557a26add28a6c9410ecb884564ddda7f97a
Chengxin Wang <w@hxdl.org>
2022-10-23 19:24:53 +0200

    Improve DKB currency conv; add trans fee

--- src/beanbot/importer/dkb.py
+++ src/beanbot/importer/dkb.py
@@ -1,6 +1,8 @@
 import csv
+from locale import currency
 import re
 from datetime import datetime, timedelta
+from typing import List
 
 from beancount.core import account, amount, data, flags
 from beancount.core.amount import Amount
@@ -42,7 +44,7 @@ class Importer(importer.ImporterProtocol):
             balance_amount = Amount(D(bal_val), 'EUR')
             balance = data.Balance(
                 meta=data.new_metadata(f.name, 4),
-                date=date_end+timedelta(days=1),
+                date=date_end+timedelta(days=1), # Beancount assumes begin-of-day balance value
                 account=self._account,
                 amount=balance_amount,
                 tolerance=None,
@@ -59,7 +61,7 @@ class Importer(importer.ImporterProtocol):
                 purpose = string_cleaning(row['Verwendungszweck'])
                 payee_account = row['Kontonummer']
                 payee_blz = row['BLZ']
-                trans_amount = row['Betrag (EUR)'].replace('.', '').replace(',', '.')
+                trans_amount = row['Betrag (EUR)']
                 sepa_creditor_id = row['Gläubiger-ID']
                 sepa_mandate_ref = row['Mandatsreferenz']
                 customer_reference = string_cleaning(row['Kundenreferenz'].strip())
@@ -75,14 +77,13 @@ class Importer(importer.ImporterProtocol):
                     date=date_booking,
                     flag=flags.FLAG_OKAY,
                     payee=payee,
-                    narration=f"Verwendungszweck: {purpose} Kundenreferenz: {customer_reference}",
+                    narration=f"Buchungstext: {booking_type} Verwendungszweck: {purpose} Kundenreferenz: {customer_reference}",
                     tags=set(),
                     links=set(),
                     postings=[],
                 )
-
                 posting = self._parse_posting(booking_type, purpose, trans_amount)
-                txn.postings.append(posting)
+                txn.postings.extend(posting)
 
                 entries.append(txn)
 
@@ -94,19 +95,50 @@ class Importer(importer.ImporterProtocol):
     def file_name(self, _):
         return "DKB_Transactions"
 
-    def _parse_posting(self, booking_type: str, trans_purpose: str, trans_amount: str) -> data.Posting:
-        posting = None
-        settle_amount = Amount(D(trans_amount), 'EUR')
+    def dec_de2intl(self, decimal: str) -> str:
+        if decimal.find(',') == -1:
+            return decimal
+        return decimal.replace('.', '').replace(',', '.')
 
-        if booking_type == 'Kartenzahlung':
-            result = parse.parse('{date:ti}{:s}Debitk.{:d} Original {orig_units} {orig_currency} 1 Euro={orig_price} {} VISA Debit', trans_purpose)
-            if result is not None:
-                orig_units = Amount(D(result['orig_units'].replace('.', '').replace(',', '.')), result['orig_currency']) # Units of foreign currency
-                orig_price = Amount(D(result['orig_price'].replace('.', '').replace(',', '.')), result['orig_currency']) # Price of 1 EUR in foreign currency
-                assert round(orig_units.number / orig_price.number, 2) == abs(settle_amount.number), f"Currency conversion doesn't match! {orig_price} / {settle_amount.currency} * {abs(settle_amount.number)} {settle_amount.currency} != {orig_units}"
-                posting = data.Posting(account=self._account, units=settle_amount, price=orig_price, cost=None, flag=None, meta=None)
+    def _parse_posting(self, booking_type: str, trans_purpose: str, trans_amount: str) -> List[data.Posting]:
+        postings = list()
+        posting_amount = Amount(D(self.dec_de2intl(trans_amount)), 'EUR')
 
-        if posting is None:
-            posting = data.Posting(account=self._account, units=settle_amount, price=None, cost=None, flag=None, meta=None)
+        fmt_foreign_trans = "Original {foreign_units} {foreign_currency:3l} 1 Euro={foreign_price} {:3l}"
+        result_foreign_trans = parse.search(fmt_foreign_trans, trans_purpose)
 
-        return posting
+        if result_foreign_trans is not None:
+            foreign_currency = result_foreign_trans['foreign_currency']
+            foreign_units = Amount(D(self.dec_de2intl(result_foreign_trans['foreign_units'])), foreign_currency) # Units of foreign currency
+            foreign_price = Amount(D(self.dec_de2intl(result_foreign_trans['foreign_price'])), foreign_currency) # Price of 1 EUR in foreign currency
+
+            fmt_trans_fee = "Fremdentgelt {fee_units} {fee_currency:3l}"
+            result_trans_fee = parse.search(fmt_trans_fee, trans_purpose)
+
+            if result_trans_fee is not None:
+                assert result_trans_fee['fee_currency'] == foreign_currency
+                trans_fee_foreign = Amount(D(self.dec_de2intl(result_trans_fee['fee_units'])), foreign_currency)
+                trans_fee = Amount(trans_fee_foreign.number / foreign_price.number, 'EUR')
+                postings = [
+                    data.Posting(account="Expenses:Others", units=trans_fee, price=foreign_price, cost=None, flag=None, meta=None),
+                    data.Posting(account=self._account, units=Amount(posting_amount.number, 'EUR'), price=foreign_price, cost=None, flag=None, meta=None)
+                ]
+                # TODO: use fee account in config
+                assert round((foreign_units.number + trans_fee_foreign.number) / foreign_price.number, 2) == abs(posting_amount.number), f"Currency conversion doesn't match!\
+                    ({foreign_units} + {trans_fee_foreign.number} (fee)) {foreign_currency} / {foreign_price} {foreign_currency}/EUR != {posting_amount.number} EUR"
+
+            else:
+                # No transaction fee
+                postings = [
+                    data.Posting(account=self._account, units=Amount(posting_amount.number, 'EUR'), price=foreign_price, cost=None, flag=None, meta=None)
+                ]
+                assert round(foreign_units.number / foreign_price.number, 2) == abs(posting_amount.number), f"Currency conversion doesn't match!\
+                    {foreign_units} / {foreign_price.number} {foreign_price.currency}/EUR != {posting_amount.number} EUR"
+
+        else:
+            # Fallback
+            postings = [
+                data.Posting(account=self._account, units=posting_amount, price=None, cost=None, flag=None, meta=None)
+            ]
+
+        return postings
