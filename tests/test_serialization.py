0021efb8684c1a9c5701d8e256c837c311f9f73d
Chengxin Wang <w@hxdl.org>
2023-12-10 10:42:15 +0100

    [WIP] add test for serialization

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -1,8 +1,8 @@
 from copy import deepcopy
 import datetime
-import pytest
+import json
 
-from beanbot.backend import data, serialization
+from beanbot.backend import serialization
 
 
 def test_serializer():
@@ -12,18 +12,18 @@ def test_serializer():
         "float": 1.0,
         "boolean": True,
         "list": [1, 2, 3],
-        "dict": {"a": 1, "b": 2},
+        "dict": {"a": 1, "b": 2, "c": [1, 2, {"d": 3}]},
         "none": None,
         "date": datetime.date(2020, 1, 1),
-        None: "null",
     }
 
     data_expected = deepcopy(data)
-    data_expected["date"] = "2020-01-01"
+    data_expected["date"] = ("___serialized_obj___", "date", ["2020-01-01"])
 
     data_serialized = serialization.serialize_dict(data)
 
     assert data_serialized == data_expected
+
 
 def test_deserializer():
     data = {
@@ -32,20 +32,33 @@ def test_deserializer():
         "float": 1.0,
         "boolean": True,
         "list": [1, 2, 3],
-        "dict": {"a": 1, "b": 2},
+        "dict": {"a": 1, "b": 2, "c": [1, 2, {"d": 3}]},
         "none": None,
-        "date": "2020-01-01",
-        None: "null",
+        "date": ("___serialized_obj___", "date", ["2020-01-01"]),
     }
 
     data_expected = deepcopy(data)
     data_expected["date"] = datetime.date(2020, 1, 1)
 
-    data_deserialized = serialization.deserialize_dict(
-        data,
-        fn_deserialize={
-            "date": lambda d: datetime.datetime.strptime(d, "%Y-%m-%d").date(),
-        },
-    )
+    data_deserialized = serialization.deserialize_object(data)
 
     assert data_deserialized == data_expected
+
+
+def test_json_compatible():
+    data = {
+        "string": "string_a",
+        "integer": 1,
+        "float": 1.0,
+        "boolean": True,
+        "list": [1, 2, 3],
+        "dict": {"a": 1, "b": 2, "c": [1, 2, {"d": 3}]},
+        "none": None,
+        "date": datetime.date(2020, 1, 1),
+    }
+
+    data_serialized = serialization.serialize_dict(deepcopy(data))
+    json_s = json.dumps(data_serialized)
+    json_d = json.loads(json_s)
+    data_deserialized = serialization.deserialize_dict(json_d)
+    assert data_deserialized == data
