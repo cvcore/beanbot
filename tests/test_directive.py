212a0503231e3ff51c8c77d884e65fa359904dbc
Chengxin Wang <w@hxdl.org>
2023-12-16 21:34:17 +0100

    add conversion to immutable directives

--- tests/test_directive.py
+++ tests/test_directive.py
@@ -17,11 +17,33 @@ def test_make_mutable():
             captured = False
             try:
                 setattr(ent, attr, "test")
-            except:
+            except AttributeError:
                 captured = True
             assert captured, f"Original entry should not be mutable!"
         print(ent)
         print(ent_mutable)
 
 
-test_make_mutable()
+def test_make_immutable():
+    for ent in bc_entries.entries:
+        ent_mutable = directive.make_mutable(ent)
+        ent_immutable = ent_mutable.to_immutable()
+        # try to change something
+        for attr in ent_immutable._asdict().keys():
+            if attr == "meta":
+                continue
+            captured = False
+            try:
+                setattr(ent_immutable, attr, "test")
+            except AttributeError:
+                captured = True
+            assert captured, f"entry should be immutable!"
+        print(ent)
+        print(ent_immutable)
+
+
+def test_equity():
+    for ent in bc_entries.entries:
+        ent_mutable = directive.make_mutable(ent)
+        ent_immutable = ent_mutable.to_immutable()
+        assert ent == ent_immutable, f"entry {ent_immutable} is not identical to the original one {ent}!"
